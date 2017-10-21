57bb07d59cd190d2d71cb2006f53626cb17645c2
Chenry <littlechenry@gmail.com>
2017-10-22 00:10:16 +0800

    小修改

--- WebContent/js/main.js
+++ WebContent/js/main.js
@@ -1,12 +1,4 @@
-var proposals = ['boat', 'bear', 'dog', 'drink', 'elephant', 'fruit'];
-
 $(document).ready(function(){
-	$('#search-form').autocomplete({
-		hints: proposals,
-		width: 300,
-		height: 30,
-		onSubmit: function(text){
-			$('#message').html('Selected: <b>' + text + '</b>');			
-		}
-	});
+    $("#page-wrapper").height($(document).height() - 131);
+    $(".content-left-list").height($(".content-right").height() - 25);
 });
